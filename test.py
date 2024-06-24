0982a6b8f30850114922450078ae1252f5fa6489
SowmeshSharma0411 <pes1202100690@pesu.pes.edu@gmail.com>
2024-06-24 13:21:31 +0530

    new project base

---
+++ test.py
@@ -0,0 +1,80 @@
+from ultralytics import YOLO
+import torch
+import time
+
+start = time.time()
+model = YOLO("yolov8n.pt")
+print(torch.cuda.is_available())
+
+results = model.track("SampleVids/bombay_trafficShortened.mp4", show=True, tracker="bytetrack.yaml", save=True)  # with ByteTrack
+end = time.time()
+length = end - start
+
+# Show the results : this can be altered however you like
+print("It took", length, "seconds!")
+
+# from collections import defaultdict
+
+# import cv2
+# import numpy as np
+
+# from ultralytics import YOLO
+# import time
+
+# start= time.time()
+
+# # Load the YOLOv8 model
+# model = YOLO("yolov8n.pt")
+
+# # Open the video file
+# video_path = "bombay_trafficShortened.mp4"
+# cap = cv2.VideoCapture(video_path)
+
+# # Store the track history
+# track_history = defaultdict(lambda: [])
+
+# # Loop through the video frames
+# while cap.isOpened():
+#     # Read a frame from the video
+#     success, frame = cap.read()
+
+#     if success:
+#         # Run YOLOv8 tracking on the frame, persisting tracks between frames
+#         results = model.track(frame, persist=True)
+
+#         # Get the boxes and track IDs
+#         boxes = results[0].boxes.xywh.cpu()
+#         track_ids = results[0].boxes.id.int().cpu().tolist()
+
+#         # Visualize the results on the frame
+#         annotated_frame = results[0].plot()
+
+#         # Plot the tracks
+#         for box, track_id in zip(boxes, track_ids):
+#             x, y, w, h = box
+#             track = track_history[track_id]
+#             track.append((float(x), float(y)))  # x, y center point
+#             if len(track) > 30:  # retain 90 tracks for 90 frames
+#                 track.pop(0)
+
+#             # Draw the tracking lines
+#             points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+#             cv2.polylines(annotated_frame, [points], isClosed=False, color=(230, 230, 230), thickness=10)
+
+#         # Display the annotated frame
+#         cv2.imshow("YOLOv8 Tracking", annotated_frame)
+
+#         # Break the loop if 'q' is pressed
+#         if cv2.waitKey(1) & 0xFF == ord("q"):
+#             break
+#     else:
+#         # Break the loop if the end of the video is reached
+#         break
+
+# # Release the video capture object and close the display window
+# cap.release()
+# cv2.destroyAllWindows()
+
+# end = time.time()
+# length = end - start
+# print("It took", length, "seconds!")
