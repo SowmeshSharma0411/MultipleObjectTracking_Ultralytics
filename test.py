384a24eec3a52f7640df8a04189705181f31f874
ShivgoudaPatil <shivgouda.h.patil@gmail.com>
2024-07-23 11:47:41 +0530

    first

--- test.py
+++ test.py
@@ -2,28 +2,16 @@ from ultralytics import YOLO
 import cv2
 from collections import defaultdict
 import numpy as np
-# import torch
 import time
 import os
 import json
 from scipy.spatial import cKDTree
 
-# start = time.time()
-# model = YOLO("yolov8n.pt")
-# print(torch.cuda.is_available())
-
-# results = model.track("SampleVids/bombay_trafficShortened.mp4", show=True, tracker="bytetrack.yaml", save=True)  # with ByteTrack
-# end = time.time()
-# length = end - start
-
-# # Show the results : this can be altered however you like
-# print("It took", length, "seconds!")
-
 start = time.time()
 
 model = YOLO("yolov8x.pt")
 
-video_path = "SampleVids/bombay_traffic.mp4"
+video_path = "Road traffic video for object recognition-Trim.mp4"
 cap = cv2.VideoCapture(video_path)
 
 # Store the track history
@@ -32,13 +20,13 @@ track_history = defaultdict(lambda: [])
 _tracker = "bytetrack.yaml"
 
 new_track_dir_no = 2
-for d in os.listdir("runs/detect"):
+for d in os.listdir("C:\\Gouda\\Capstone\\Segmentation\\YOLOV8\\MultipleObjectTracking_Ultralytics\\Tracks"):
     if d.startswith("track"):
         new_track_dir_no = max(int(d[5:]), new_track_dir_no)
 
 new_track_dir_no += 1
 new_track_dir = f"track{new_track_dir_no}"
-new_track_path = os.path.join("runs/detect", new_track_dir)
+new_track_path = os.path.join("C:\\Gouda\\Capstone\\Segmentation\\YOLOV8\\MultipleObjectTracking_Ultralytics\\Tracks", new_track_dir)
 os.makedirs(new_track_path)
 
 output_video_path = os.path.join(new_track_path, "output.mp4")
@@ -52,11 +40,6 @@ frame_stride = 20  # Number of frames to skip between each detection
 
 i = 0
 
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = int(cap.get(cv2.CAP_PROP_FPS))
-
-
 # Loop through the video frames
 while cap.isOpened():
     success, frame = cap.read()
@@ -69,9 +52,8 @@ while cap.isOpened():
 
         annotated_frame = results[0].plot()
 
-        if (i % frame_stride == 0):
-
-            # This is code for veclocity and nearby objects: the next 2 lines: they r crucial research on this
+        if i % frame_stride == 0:
+            # This is code for velocity and nearby objects: the next 2 lines: they are crucial research on this
             all_positions = [(float(box[0]), float(box[1])) for box in boxes]
             tree = cKDTree(all_positions)
 
@@ -79,7 +61,7 @@ while cap.isOpened():
                 x, y, w, h = box
                 track = track_history[track_id]
 
-                if (len(track) > 0):
+                if len(track) > 0:
                     prev_x, prev_y = track[-1][0], track[-1][1]
                     delta_x, delta_y = float(x) - prev_x, float(y) - prev_y
 
@@ -91,12 +73,10 @@ while cap.isOpened():
                     velocity = 0.0
 
                 # Count nearby objects (excluding itself)
-                nearby_objects_count = len(
-                    tree.query_ball_point((float(x), float(y)), r=50)) - 1
+                nearby_objects_count = len(tree.query_ball_point((float(x), float(y)), r=50)) - 1
 
-                # track.append((float(x), float(y), float(w), float(h)))
-                track.append((float(x), float(y), float(w),
-                              float(h), delta_x, delta_y))
+                # Append all calculated values to the track
+                track.append((float(x), float(y), float(w), float(h), delta_x, delta_y, velocity, nearby_objects_count))
 
         out.write(annotated_frame)
 
@@ -115,7 +95,7 @@ cap.release()
 out.release()
 cv2.destroyAllWindows()
 
-
+# Save the track history to a JSON file
 track_history_dict = {str(track_id): [
     {
         "x": point[0],
